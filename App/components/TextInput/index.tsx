41111cd8d0d8f5794a19c906d0161cafd46a8289
Gridan Gabriel <gabriel.gridan@sensidev.com>
2018-03-16 14:07:08 +0200

    MD-132 Correct error in TextInput

--- App/components/TextInput/index.tsx
+++ App/components/TextInput/index.tsx
@@ -152,7 +152,7 @@ export class SXTextInput extends Component<ISXTextInputProps, ISXTextInputState>
 			return (
 				<TouchableOpacity style={style.cancelButton} onPress={() => Keyboard.dismiss()}>
 					<Text style={[style.cancelButtonText, {color: this.props.cancelButtonTextColor}]}>Cancel</Text>
-				</<Text style={[st>
+				</TouchableOpacity>
 			);
 		}
 		return null;
