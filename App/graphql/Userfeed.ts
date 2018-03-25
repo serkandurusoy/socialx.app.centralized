58b3fb8d99f2e0378805f8bc641e79aef0184927
jake <imedevil@yahoo.com>
2018-03-25 23:21:55 +0200

    ipfs <-> appsync / create media / display ipfs media on posts

--- App/graphql/Userfeed.ts
+++ App/graphql/Userfeed.ts
@@ -4,52 +4,73 @@ import {graphql, QueryProps} from 'react-apollo';
 import {CurrentUserInfo} from '../utils';
 
 export const createPostMut = gql`
-    mutation($text: String!, $Media: String){
-        createPost(text: $text, Media: $Media){
-            id
-        }
-    }
+	mutation($text: String!, $Media: [ID]) {
+		createPost(text: $text, Media: $Media) {
+			id
+		}
+	}
+`;
+
+export const addMediaMut = gql`
+	mutation($hash: String!, $type: String!, $size: Int!) {
+		addMedia(hash: $hash, type: $type, size: $size) {
+			id
+		}
+	}
 `;
 
 export const getUserPosts = gql`
-    query{
-        user {
-            userId
-            posts {
-                id
-                createdAt
-                text
-                Media
-                likes {
-                    userId
-                    username
-                    avatar
-                }
-            }
-        }
-    }
+	query {
+		user {
+			userId
+			posts {
+				id
+				createdAt
+				text
+				Media {
+					id
+					hash
+					type
+					size
+				}
+				likes {
+					userId
+					username
+					avatar
+				}
+			}
+		}
+	}
 `;
 
 export const getAllPosts = gql`
-    query{
-        allPosts {
-            id
-            createdAt
-            text
-            Media
-            likes {
-                userId
-                username
-                avatar
-            }
-        }
-    }
+	query {
+		allPosts {
+			id
+			createdAt
+			text
+			Media {
+				id
+				hash
+				type
+				size
+			}
+			likes {
+				userId
+				username
+				avatar
+			}
+		}
+	}
 `;
 
-export const getAllPostsHoc = (comp: any) => graphql(getAllPosts,
-		{ name: 'Posts', options: { fetchPolicy: 'network-only' } })(comp);
+export const getAllPostsHoc = (comp: any) =>
+	graphql(getAllPosts, {name: 'Posts', options: {fetchPolicy: 'network-only'}})(comp);
 
-export const getUserPostsHoc = (comp: any) => graphql(getUserPosts,
-		{ name: 'User', options: { fetchPolicy: 'network-only' } })(comp);
+export const getUserPostsHoc = (comp: any) =>
+	graphql(getUserPosts, {name: 'User', options: {fetchPolicy: 'network-only'}})(comp);
 
-export const createPostHoc = (comp: any) => graphql(createPostMut, { name: 'createPost' })(comp);
+export const addMediaHoc = (comp: any) =>
+	graphql(addMediaMut, {name: 'addMedia', options: {fetchPolicy: 'no-cache'}})(comp);
+
+export const createPostHoc = (comp: any) => graphql(createPostMut, {name: 'createPost'})(comp);
