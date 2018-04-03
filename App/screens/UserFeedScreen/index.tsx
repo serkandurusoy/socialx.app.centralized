7288186298d55ecdff8fe58fce7f1be24da76a50
jake <imedevil@yahoo.com>
2018-04-03 19:01:39 +0300

    userfeed - hotfix allposts

--- App/screens/UserFeedScreen/index.tsx
+++ App/screens/UserFeedScreen/index.tsx
@@ -91,6 +91,11 @@ class UserFeedScreen extends Component<IUserFeedScreenProps, IUserFeedScreenStat
 	private getWallPosts = () => {
 		const {data, Posts} = this.props;
 		const arty: any[] = [];
+
+		if (!Posts.allPosts) {
+			return arty;
+		}
+
 		for (let i = 0; i < Posts.allPosts.length; i++) {
 			const post = Posts.allPosts[i];
 			const media = post.Media
