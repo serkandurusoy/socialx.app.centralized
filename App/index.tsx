e61bf2824fbbef3661d228058de429e498b4c503
ionutmovila <ionut.movila@sensidev.com>
2018-04-05 16:38:27 +0300

    MD-164, first version with splash screen

--- App/index.tsx
+++ App/index.tsx
@@ -1,20 +1,29 @@
 import React, {Component} from 'react';
 import {ApolloProvider} from 'react-apollo';
+import {StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import {Provider} from 'react-redux';
 
 // refactoring
 import RootContainer, {AppsyncClient, Rehydrated} from './containers/RootContainer';
 import createStore from './reducers';
+import {Colors} from './theme';
 
 const store = createStore();
 
 export default class App extends Component<{}, {}> {
+
+	public componentDidMount(): void {
+		StatusBar.setBackgroundColor(Colors.pink);
+		SplashScreen.hide();
+	}
+
 	public render() {
 		return (
 			<ApolloProvider client={AppsyncClient}>
 				<Rehydrated>
 					<Provider store={store}>
-						<RootContainer />
+						<RootContainer/>
 					</Provider>
 				</Rehydrated>
 			</ApolloProvider>
--- App/screens/LaunchScreen/index.tsx
+++ App/screens/LaunchScreen/index.tsx
@@ -1,20 +1,17 @@
 import React, {Component} from 'react';
 import {Image, Text, View} from 'react-native';
 import {NavigationScreenProp} from 'react-navigation';
-import {connect} from 'react-redux';
-import {SXButton, SXGradientButton, TextGradient} from '../../components';
+import {SXButton} from '../../components/Button';
+import {SXGradientButton} from '../../components/GradientButton';
+import {TextGradient} from '../../components/TextGradient';
 import {Colors, Images} from '../../theme';
 import style from './style';
 
-import {hideActivityIndicator, showActivityIndicator} from '../../actions';
-
 export interface ILaunchScreenProps {
 	navigation: NavigationScreenProp<any>;
-	showActivityIndicator: () => void;
-	hideActivityIndicator: () => void;
 }
 
-class LaunchScreen extends Component<ILaunchScreenProps, any> {
+export default class LaunchScreen extends Component<ILaunchScreenProps, any> {
 	private static navigationOptions = {
 		header: null,
 	};
@@ -22,9 +19,10 @@ class LaunchScreen extends Component<ILaunchScreenProps, any> {
 	public render() {
 		return (
 			<View style={style.container}>
-				<Image source={Images.launch_screen_bg} style={style.background} resizeMode={'cover'} />
+				<Image source={Images.launch_screen_bg} style={style.background} resizeMode={'cover'}/>
 				<View style={style.topPaddingContainer}>
-					<TextGradient text={'SocialX'} colors={[Colors.fuchsiaBlue, Colors.pink]} style={style.socialxGradient} />
+					<TextGradient text={'SocialX'} colors={[Colors.fuchsiaBlue, Colors.pink]}
+								  style={style.socialxGradient}/>
 					<Text style={style.description}>Social interaction with cryptocurrency rewards</Text>
 				</View>
 				<TextGradient
@@ -41,7 +39,7 @@ class LaunchScreen extends Component<ILaunchScreenProps, any> {
 						onPress={this.navigateToLoginScreen}
 					/>
 					<View style={style.signUpTopPadding}>
-						<SXButton label={'SIGN UP'} onPress={this.navigateToSignUpScreen} />
+						<SXButton label={'SIGN UP'} onPress={this.navigateToSignUpScreen}/>
 					</View>
 				</View>
 			</View>
@@ -56,10 +54,3 @@ class LaunchScreen extends Component<ILaunchScreenProps, any> {
 		this.props.navigation.navigate('SignUpScreen');
 	}
 }
-
-const mapDispatchToProps = (dispatch: any): Partial<ILaunchScreenProps> => ({
-	showActivityIndicator: () => dispatch(showActivityIndicator('Simple')),
-	hideActivityIndicator: () => dispatch(hideActivityIndicator()),
-});
-
-export default connect(null, mapDispatchToProps)(LaunchScreen);
