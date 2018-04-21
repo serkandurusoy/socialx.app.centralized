f30323e99096d139d757271dea5ba3b6657abde7
jake <imedevil@yahoo.com>
2018-04-22 02:44:06 +0300

    add(commentsReply): full functionality

--- App/screens/CommentsScreen/RepliesScreen/index.tsx
+++ App/screens/CommentsScreen/RepliesScreen/index.tsx
@@ -1,33 +1,160 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import {NavigationScreenProp, NavigationStackScreenOptions} from 'react-navigation';
+import {connect} from 'react-redux';
 import RepliesScreenComponent from './screen';
+
+import {commentHoc, getCommentsHoc, likeCommentHoc, removeCommentLikeHoc} from '../../../graphql';
+
+import {IComments, ICommentsResponse} from '../../../types/gql';
+import {IWallPostCommentReply} from '../index';
+
+import {hideActivityIndicator, showActivityIndicator} from '../../../actions';
+
+import base from '../../../config/ipfs';
+
+const imagePlaceHolder = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png';
 
 interface IRepliesScreenProps {
 	navigation: NavigationScreenProp<any>;
+
+	getComments: any;
+	comment: any;
+
+	likeComment: any;
+	removeCommentLike: any;
+
+	loadCommenting: () => void;
+	hideLoader: () => void;
 }
 
-export default class RepliesScreen extends Component<IRepliesScreenProps> {
+interface IRepliesScreenState {
+	replies: IWallPostCommentReply[];
+}
+
+class RepliesScreen extends Component<IRepliesScreenProps, IRepliesScreenState> {
 	private static navigationOptions: Partial<NavigationStackScreenOptions> = {
 		title: 'Replies',
 		headerRight: <View />,
 	};
 
+	public state = {
+		replies: [],
+	};
+
+	public async componentDidMount() {
+		await this.preFetchComments();
+	}
+
 	public render() {
+		const {replies} = this.state;
 		return (
 			<RepliesScreenComponent
-				{...this.props.navigation.state.params}
+				replies={replies}
+				startReply={this.props.navigation.state.params.startReply}
 				onReplyLike={this.onReplyLikeHandler}
 				onSendReply={this.onSendReplyHandler}
 			/>
 		);
 	}
 
-	private onReplyLikeHandler = () => {
-		// console.log('onReplyLikeHandler');
+	private onReplyLikeHandler = async (comment: IWallPostCommentReply) => {
+		const {likeComment, removeCommentLike} = this.props;
+
+		console.log('cmt', comment);
+		const mVar = {variables: {commentId: comment.id}};
+		try {
+			if (comment.likedByMe) {
+				await removeCommentLike(mVar);
+			} else {
+				await likeComment(mVar);
+			}
+			await this.preFetchComments();
+		} catch (ex) {
+			console.log(ex);
+		}
 	}
 
-	private onSendReplyHandler = (replyText: string) => {
+	private onSendReplyHandler = async (replyText: string) => {
+		const {loadCommenting, hideLoader, comment, navigation} = this.props;
+		const commentId = navigation.state.params.commentId;
+
+		const mVar = {variables: {targetComment: commentId, text: replyText}};
+		loadCommenting();
+		try {
+			await comment(mVar);
+			await this.preFetchComments();
+		} catch (ex) {
+			console.log(ex);
+		}
+		hideLoader();
 		// console.log('onSendReplyHandler', replyText);
 	}
+
+	private preFetchComments = async () => {
+		const commentId = this.props.navigation.state.params.commentId;
+		const userId = this.props.navigation.state.params.userId;
+		const {getComments} = this.props;
+
+		const qVar = {variables: {targetComment: commentId}};
+		try {
+			const getResp: ICommentsResponse = await getComments(qVar);
+
+			if (getResp.data.getComments.length <= 0) {
+				return;
+			}
+
+			const comments: IComments[] = getResp.data.getComments;
+			const resComments: IWallPostCommentReply[] = [];
+
+			for (let i = 0; i < comments.length; i++) {
+				const currentComment = comments[i];
+				const commentOwnerAv = currentComment.owner.avatar
+				? base.ipfs_URL + currentComment.owner.avatar.hash
+				: imagePlaceHolder;
+
+				resComments.push({
+					id: currentComment.id,
+					text: currentComment.text,
+					user: {
+						fullName: currentComment.owner.name,
+						avatarURL: commentOwnerAv,
+					},
+					timestamp: new Date(currentComment.createdAt),
+					likes: currentComment.likes,
+					numberOfLikes: currentComment.likes.length,
+					likedByMe: currentComment.likes.find((x) => x.userId === userId) ? true : false,
+				});
+			}
+
+			this.setState({
+				replies: resComments.sort((a: any, b: any) => {
+					if (a.numberOfLikes > 0 || b.numberOfLikes > 0) {
+						a = a.numberOfLikes;
+						b = b.numberOfLikes;
+						return a > b ? -1 : a < b ? 1 : 0;
+					}
+					a = a.timestamp;
+					b = b.timestamp;
+					return a > b ? -1 : a < b ? 1 : 0;
+				}),
+			});
+		} catch (ex) {
+			return;
+		}
+	}
 }
+
+const MapDispatchToProps = (dispatch: any) => ({
+	loadCommenting: () => dispatch(showActivityIndicator('Submitting comment..')),
+	hideLoader: () => dispatch(hideActivityIndicator()),
+});
+
+const reduxWrapper = connect(null, MapDispatchToProps)(RepliesScreen as any);
+
+const getCommentsWrapper = getCommentsHoc(reduxWrapper);
+const commentWrapper = commentHoc(getCommentsWrapper);
+const likeWrapper = likeCommentHoc(commentWrapper);
+const removeLikeWrapper = removeCommentLikeHoc(likeWrapper);
+
+export default removeLikeWrapper;
