a12b9120761817dd911fd858dbcd2592ecae82f5
jake <imedevil@yahoo.com>
2018-04-22 03:16:14 +0300

    feature(removePost): full functionality

--- App/graphql/Post.ts
+++ App/graphql/Post.ts
@@ -2,29 +2,38 @@ import gql from 'graphql-tag';
 import {graphql, QueryProps} from 'react-apollo';
 
 const likePost = gql`
-    mutation likePost($postId: ID!){
-        likePost(postId: $postId){
-            id,
-            likes {
-			    userId,
-			    username
+	mutation likePost($postId: ID!) {
+		likePost(postId: $postId) {
+			id
+			likes {
+				userId
+				username
 			}
-        }
-    }
+		}
+	}
 `;
 
 const removeLikePost = gql`
-    mutation removelikePost($postId: ID!){
-        removelikePost(postId: $postId){
-            id,
-            likes {
-			    userId,
-			    username
+	mutation removelikePost($postId: ID!) {
+		removelikePost(postId: $postId) {
+			id
+			likes {
+				userId
+				username
 			}
-        }
-    }
+		}
+	}
 `;
 
-export const likePostHoc = (comp: any) => graphql(likePost, { name: 'likePost' })(comp);
+const deletePostMut = gql`
+	mutation deletePost($postId: ID!) {
+		deletePost(postId: $postId) {
+			id
+		}
+	}
+`;
 
-export const removeLikePostHoc = (comp: any) => graphql(removeLikePost, { name: 'removeLikePost' })(comp);
+export const likePostHoc = (comp: any) => graphql(likePost, {name: 'likePost'})(comp);
+export const removeLikePostHoc = (comp: any) => graphql(removeLikePost, {name: 'removeLikePost'})(comp);
+
+export const deleteOwnPostHoc = (comp: any) => graphql(deletePostMut, {name: 'deletePost'})(comp);
