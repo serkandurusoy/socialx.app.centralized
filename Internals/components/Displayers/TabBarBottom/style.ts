12e290431eb9dec842eda8ac5b6c9d833db84119
ionutmovila <ionut.movila@sensidev.com>
2018-05-14 12:11:42 +0300

    SOC-65, notifications bottom menu badge

--- Internals/components/Displayers/TabBarBottom/style.ts
+++ Internals/components/Displayers/TabBarBottom/style.ts
@@ -1,7 +1,8 @@
 import {StyleSheet} from 'react-native';
-import {Colors, Sizes} from 'theme';
+import {Colors, Fonts, Sizes} from 'theme';
 
 const ICON_PADDING = Sizes.smartVerticalScale(12);
+const BADGE_HEIGHT = Sizes.smartHorizontalScale(18);
 
 const style: any = {
 	container: {
@@ -21,6 +22,27 @@ const style: any = {
 		position: 'absolute',
 		top: ICON_PADDING,
 		left: ICON_PADDING,
+	},
+	notificationsContainer: {
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	notificationBadge: {
+		...Fonts.centuryGothic,
+		fontSize: Sizes.smartHorizontalScale(16),
+		lineHeight: BADGE_HEIGHT,
+		color: Colors.white,
+	},
+	badgeBackground: {
+		position: 'absolute',
+		borderRadius: BADGE_HEIGHT / 2,
+		minWidth: BADGE_HEIGHT,
+		paddingHorizontal: Sizes.smartHorizontalScale(4),
+		backgroundColor: Colors.red,
+		alignItems: 'center',
+		left: '55%',
+		top: 0,
 	},
 };
 
--- Internals/types/appUI.ts
+++ Internals/types/appUI.ts
@@ -1,15 +1,13 @@
 export interface IAppUIStateProps {
 	tabBarBottomHeight: number;
-}
-
-export interface ITabBarBottomHeight {
-	tabBarBottomHeight: number;
+	newNotifications: number;
 }
 
 export interface IAppUIAction {
 	type: string;
-	payload?: ITabBarBottomHeight;
+	payload?: Partial<IAppUIStateProps>;
 }
 
 export const UPDATE_TAB_BAR_BOTTOM_HEIGHT = 'update_tab_bar_bottom_height';
 export const START_APP = 'start_app';
+export const UPDATE_NEW_NOTIFICATIONS = 'update_new_notifications';
