f8ce76b47a4f73056ea1fc9cbc22583af7256605
jake <imedevil@yahoo.com>
2018-05-17 07:30:47 +0300

    rem todo

--- App/initializers/connection.ts
+++ App/initializers/connection.ts
@@ -1,7 +1,6 @@
 import {ConnectionInfo, ConnectionType} from 'react-native';
 
 export default (connection: ConnectionInfo) => {
-	// @ionut TODO: display to the user that their connection is lost etc..
 	if (connection.type === 'NONE' || connection.type === 'unknown') {
 		// no internet
 	} else {
