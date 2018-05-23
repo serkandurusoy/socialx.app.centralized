4f809fb134b668c8dc56681aa870a446dbf3bf34
ionutmovila <ionut.movila@sensidev.com>
2018-05-23 14:41:34 +0300

    SOC-81, revert to old android tools + SDK

--- App/initializers/index.ts
+++ App/initializers/index.ts
@@ -2,5 +2,14 @@ import {languageInit} from 'utilities/amplify';
 
 export default () => {
 	// init I18n language set
+	removeConsoleLogs();
 	languageInit();
+};
+
+const removeConsoleLogs = () => {
+	if (!__DEV__) {
+		// tslint:disable-next-line
+		console.log = () => {
+		};
+	}
 };
