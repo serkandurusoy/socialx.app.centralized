09f641fe1172194998ab7cabb2b78e588ef6ecfb
ionutmovila <ionut.movila@sensidev.com>
2018-05-23 16:37:29 +0300

    Fixed static html files

--- App/screens/MediaLicenceFAQScreen/index.tsx
+++ App/screens/MediaLicenceFAQScreen/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import {SafeAreaView, View, WebView} from 'react-native';
+import {Platform, SafeAreaView, View, WebView} from 'react-native';
+
+import {OS_TYPES} from 'consts';
 import MediaLicenceFAQHTML from './media_licence_faq.html';
 import style from './style';
 
 const MediaLicenceFAQScreen: React.SFC = () => {
+	const webViewLocalSource = Platform.OS === OS_TYPES.IOS ?
+		MediaLicenceFAQHTML : {uri: 'file:///android_asset/html/media_licence_faq.html'};
 	return (
 		<SafeAreaView style={style.container}>
-			<WebView source={MediaLicenceFAQHTML} />
+			<WebView source={webViewLocalSource} />
 		</SafeAreaView>
 	);
 };
--- App/screens/TermsAndConditionsScreen/index.tsx
+++ App/screens/TermsAndConditionsScreen/index.tsx
@@ -3,12 +3,11 @@ import {Platform, SafeAreaView, View, WebView} from 'react-native';
 
 import {OS_TYPES} from 'consts';
 import style from './style';
+import TermsAndConditionsHTML from './terms_and_conditions.html';
 
 const TermsAndConditionsScreen: React.SFC = () => {
-	const webViewLocalSource =
-		Platform.OS === OS_TYPES.IOS
-			? TermsAndConditionsHTML
-			: {uri: 'file:///android_asset/html/terms_and_conditions.html'};
+	const webViewLocalSource = Platform.OS === OS_TYPES.IOS ?
+		TermsAndConditionsHTML : {uri: 'file:///android_asset/html/terms_and_conditions.html'};
 	return (
 		<SafeAreaView style={style.container}>
 			<WebView source={webViewLocalSource}/>
@@ -18,7 +17,7 @@ const TermsAndConditionsScreen: React.SFC = () => {
 
 TermsAndConditionsScreen.navigationOptions = {
 	title: 'Terms and Conditions',
-	headerRight: <View />,
+	headerRight: <View/>,
 };
 
 export default TermsAndConditionsScreen;
