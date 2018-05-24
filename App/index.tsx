330d742855b870d82dcf479938ad19cc13258552
serkandurusoy <serkan.durusoy@dna-tr.com>
2018-05-24 18:36:55 +0300

    ask some questions and code format

--- App/index.tsx
+++ App/index.tsx
@@ -10,35 +10,37 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 
 // refactoring
-import {getAvailableAnimations} from 'configuration/animations';
+import { Animations } from 'configuration/animations';
 import {OS_TYPES} from 'consts';
 import {Colors} from 'theme';
 
 import {AppsyncClient, Rehydrated} from 'backend/appsync';
 import RootContainer from './containers/RootContainer';
 
-import createStore from './reducers';
+import createStore from './reducers/createStore';
 
-// TODO: find a proper suitable initilizers invoker that is high level and equivalent of this
+// TODO: find a proper suitable initializers invoker that is high level and equivalent of this
 import Init from './initializers';
+// TODO: @serkan ask @jake why here, why not in the component?
+// TODO: besides this won't always work because languageInit is async so it should be "await"ed in componentDidMount
 Init();
 
-const reduxStage = createStore();
+const { store, persistor } = createStore();
 
 export default class App extends Component<{}, {}> {
 	public componentDidMount(): void {
 		if (Platform.OS === OS_TYPES.Android) {
 			StatusBar.setBackgroundColor(Colors.pink);
 		}
-		Animatable.initializeRegistryWithDefinitions(getAvailableAnimations());
+		Animatable.initializeRegistryWithDefinitions(Animations);
 		Orientation.lockToPortrait();
 	}
 
 	public render() {
-		const store = reduxStage.store;
-		const persistor = reduxStage.persistor;
 		return (
 			<ApolloProvider client={AppsyncClient}>
+				{/* // TODO: @serkan ask @jake why not use PersistGate? */}
+				{/* // TODO: and looks like aws-appsync-react is an "exotic" module, not officially installed from npm */}
 				<Rehydrated>
 					<Provider store={store}>
 						<RootContainer />
