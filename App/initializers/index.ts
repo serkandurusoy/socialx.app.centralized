cf71791656c148a818f49c77c469aaef9064d6d8
jake <imedevil@yahoo.com>
2018-05-25 11:21:45 +0300

    SOC-90: added the dif

--- App/initializers/index.ts
+++ App/initializers/index.ts
@@ -1,6 +1,14 @@
 import {languageInit} from 'utilities/amplify';
 
 export default () => {
+	removeConsoleLogs();
 	// init I18n language set
 	languageInit();
+};
+
+const removeConsoleLogs = () => {
+	if (!__DEV__) {
+		// tslint:disable-next-line
+		console.log = () => {};
+	}
 };
--- App/screens/TermsAndConditionsScreen/index.tsx
+++ App/screens/TermsAndConditionsScreen/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import {SafeAreaView, View, WebView} from 'react-native';
+import {Platform, SafeAreaView, View, WebView} from 'react-native';
+
+import {OS_TYPES} from 'consts';
 import style from './style';
-import TermsAndConditionsHTML from './terms_and_conditions.html';
 
 const TermsAndConditionsScreen: React.SFC = () => {
+	const webViewLocalSource =
+		Platform.OS === OS_TYPES.IOS
+			? TermsAndConditionsHTML
+			: {uri: 'file:///android_asset/html/terms_and_conditions.html'};
 	return (
 		<SafeAreaView style={style.container}>
-			<WebView source={TermsAndConditionsHTML}/>
+			<WebView source={webViewLocalSource}/>
 		</SafeAreaView>
 	);
 };
 
 TermsAndConditionsScreen.navigationOptions = {
 	title: 'Terms and Conditions',
-	headerRight: <View/>,
+	headerRight: <View />,
 };
 
 export default TermsAndConditionsScreen;
