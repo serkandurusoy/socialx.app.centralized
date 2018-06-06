bb616840188bed8af2646e9d338295fef373504e
ionutmovila <ionut.movila@sensidev.com>
2018-06-06 15:34:30 +0300

    SOC-162, fixed the problem with input code on Android not showing

--- Internals/components/Modals/InputSMSCode/index.tsx
+++ Internals/components/Modals/InputSMSCode/index.tsx
@@ -60,7 +60,7 @@ class ModalInputSMSCodeComponent extends Component<IModalInputSMSCodeComponentPr
 					<View style={style.borderContainer}>
 						<Text style={style.message}>{'Please type the verification code sent to ' + this.props.phoneNumber}</Text>
 						<View style={style.inputCellsContainer}>
-							{[...Array(NUMBER_OF_DIGITS).keys()].map((i) => (
+							{[...Array(NUMBER_OF_DIGITS)].map((i) => (
 								<TextInput
 									key={i}
 									onChangeText={(value: string) => this.digitUpdatedHandler(i, value)}
