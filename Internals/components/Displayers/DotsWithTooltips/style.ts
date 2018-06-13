57e7a0b8a2cacb7e61dd2e2a8fa69a0677311794
ionutmovila <ionut.movila@sensidev.com>
2018-06-13 16:32:54 +0300

    MD-252, my profile pull to refresh + updated top right menu button

--- Internals/components/Displayers/DotsWithTooltips/style.ts
+++ Internals/components/Displayers/DotsWithTooltips/style.ts
@@ -9,16 +9,20 @@ const style: any = {
 		alignItems: 'center',
 		flexDirection: 'row',
 	},
-	dots: {
-		height: Sizes.smartVerticalScale(5),
-		width: Sizes.smartHorizontalScale(19),
+	dotsIcon: {
+		fontSize: Sizes.smartHorizontalScale(25),
 	},
 	icon: {
 		marginLeft: Sizes.smartHorizontalScale(5),
 		marginRight: Sizes.smartHorizontalScale(10),
 	},
+	fontIcon: {
+		fontSize: Sizes.smartHorizontalScale(22),
+	},
 	label: {
 		...Fonts.centuryGothic,
+		fontSize: Sizes.smartHorizontalScale(16),
+		lineHeight: Sizes.smartHorizontalScale(22),
 		color: Colors.postFullName,
 		textAlign: 'left',
 	},
