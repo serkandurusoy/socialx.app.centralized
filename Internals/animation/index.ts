9b4151ef370d822bbea79dde8506d60bba969a88
ionutmovila <ionut.movila@sensidev.com>
2018-06-15 14:50:27 +0300

    MD-268, globe animation for loading screens

--- Internals/animation/index.ts
+++ Internals/animation/index.ts
@@ -1,2 +1,3 @@
 export const dragon = require('./assets/dragonAnimation.json');
 export const globe = require('./assets/globeAnimation.json');
+export const globe2 = require('./assets/globe2Animation.json');
