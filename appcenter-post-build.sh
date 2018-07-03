2793a1dc2648ac4dbeca78b3b98a5856d5c530cc
ionutmovila <ionut.movila@sensidev.com>
2018-07-03 15:46:12 +0300

    MD-300, one more round fixes for BugSnag upload

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -2,18 +2,17 @@
 
 API_KEY='73245fce110f157e3c5ba0c2ac7154ae'
 ROOT_DIR=${APPCENTER_SOURCE_DIRECTORY}
-OUTPUT_DIR=${APPCENTER_OUTPUT_DIRECTORY}
+SOURCE_DIR=${APPCENTER_SOURCE_DIRECTORY}
 BUILD_ID=${APPCENTER_BUILD_ID}
 echo "App root dir ${ROOT_DIR}"
 
 uploadAndroid() {
-    ANDROID_BUNDLE_NAME='android-release.bundle' # look for 'bundleAssetName' in android/app/build.gradle
-    ANDROID_MAP_NAME='android-release.bundle.map' # look for '--sourcemap-output' in android/app/build.gradle
+    ANDROID_BUNDLE_NAME='index.android.bundle' # look for 'bundleAssetName' in android/app/build.gradle
+    ANDROID_MAP_NAME='index.android.bundle.map' # look for '--sourcemap-output' in android/app/build.gradle
     MINIFIED_FILE="${ROOT_DIR}/android/app/build/intermediates/assets/release/${ANDROID_BUNDLE_NAME}"
     SOURCE_MAP_FILE="${ROOT_DIR}/${ANDROID_MAP_NAME}"
     ANDROID_APP_VERSION=$((BUILD_ID  * 10 + 1))
 
-    echo 'About to upload sourcemaps to BugSnag'
     echo "Minified file: ${MINIFIED_FILE}"
     echo "Source map file: ${SOURCE_MAP_FILE}"
 
@@ -25,21 +24,18 @@ uploadAndroid() {
         --source-map ${SOURCE_MAP_FILE} \
         --minified-url index.android.bundle \
         --upload-sources
-
-    echo 'BugSnag upload complete'
 }
 
 uploadIOS() {
     IOS_BUNDLE_NAME='main.jsbundle'
     IOS_MAP_NAME='index.ios.map' # not sure about the path here...
-    MINIFIED_FILE="${OUTPUT_DIR}/${IOS_BUNDLE_NAME}"
+    MINIFIED_FILE="${SOURCE_DIR}/${IOS_BUNDLE_NAME}"
     SOURCE_MAP_FILE="${ROOT_DIR}/${IOS_MAP_NAME}"
     IOS_APP_VERSION=$((BUILD_ID  * 10 + 2))
 
     echo 'Listing ROOT_DIR'
     ls -la ${ROOT_DIR}
 
-    echo 'About to upload sourcemaps to BugSnag'
     echo "Minified file: ${MINIFIED_FILE}"
     echo "Source map file: ${SOURCE_MAP_FILE}"
 
@@ -51,8 +47,6 @@ uploadIOS() {
         --source-map ${SOURCE_MAP_FILE} \
         --minified-url main.jsbundle \
         --upload-sources
-
-    echo 'BugSnag upload complete'
 }
 
 if [ -n "${APPCENTER_XCODE_SCHEME}" ]; then
