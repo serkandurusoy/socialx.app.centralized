b83673b0b571a06ab6dd93dff0cb997156841417
ionutmovila <ionut.movila@sensidev.com>
2018-07-03 12:32:25 +0300

    MD-300, android integrate BugSnag from AppCenter - 2

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-if [ -n ${APPCENTER_ANDROID_VARIANT} ]; then
-    uploadAndroid
-else
-    echo 'TODO: iOS build'
-fi
+API_KEY='73245fce110f157e3c5ba0c2ac7154ae'
+ROOT_DIR=${APPCENTER_SOURCE_DIRECTORY}
+echo "App root dir ${ROOT_DIR}"
 
 uploadAndroid() {
-    ROOT_DIR=${APPCENTER_SOURCE_DIRECTORY}
-    echo "App root dir ${ROOT_DIR}"
-
-    API_KEY='73245fce110f157e3c5ba0c2ac7154ae'
     ANDROID_BUNDLE_NAME='android-release.bundle' # look for 'bundleAssetName' in android/app/build.gradle
     ANDROID_MAP_NAME='android-release.bundle.map' # look for '--sourcemap-output' in android/app/build.gradle
     MINIFIED_FILE="${ROOT_DIR}/android/app/build/intermediates/assets/release/${ANDROID_BUNDLE_NAME}"
@@ -32,3 +26,37 @@ uploadAndroid() {
 
     echo 'BugSnag upload complete'
 }
+
+uploadIOS() {
+    IOS_BUNDLE_NAME='main.jsbundle'
+    IOS_MAP_NAME='index.ios.map'
+    MINIFIED_FILE="${ROOT_DIR}/${IOS_BUNDLE_NAME}"
+    SOURCE_MAP_FILE="${ROOT_DIR}${IOS_MAP_NAME}"
+    APP_VERSION='1.0.6'
+
+    echo 'Listing ROOT_DIR'
+    ls -la ${ROOT_DIR}
+
+    echo 'About to upload sourcemaps to BugSnag'
+    echo "Minified file: ${MINIFIED_FILE}"
+    echo "Source map file: ${SOURCE_MAP_FILE}"
+
+    cd ${ROOT_DIR}
+    ./node_modules/bugsnag-sourcemaps/cli.js upload \
+        --api-key YOUR_API_KEY_HERE \
+        --app-version ${APP_VERSION} \
+        --minified-file ${MINIFIED_FILE} \
+        --source-map ${SOURCE_MAP_FILE} \
+        --minified-url main.jsbundle \
+        --upload-sources
+
+    echo 'BugSnag upload complete'
+}
+
+if [ -n ${APPCENTER_ANDROID_VARIANT} ]; then
+    echo 'Now uploadAndroid'
+    uploadAndroid
+else
+    echo 'Now uploadIOS'
+   uploadIOS
+fi
