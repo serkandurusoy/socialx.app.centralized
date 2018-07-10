b35017c6535098f778cc9f96a381916d2533b39d
ionutmovila <ionut.movila@sensidev.com>
2018-07-10 14:10:06 +0300

    MD-330, refactoring Avatar folder + first 2 components in Displayers

--- Internals/components/Displayers/AddFriendsList/index.tsx
+++ Internals/components/Displayers/AddFriendsList/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Image, ScrollView, TouchableOpacity, View} from 'react-native';
+
 import {Icons} from 'theme';
 import {FriendsSearchResult} from 'types';
 import style from './style';
@@ -9,22 +10,20 @@ export interface IAddFriendsListProps {
 	taggedFriends: FriendsSearchResult[];
 }
 
-export const AddFriendsList: React.SFC<IAddFriendsListProps> = (props) => {
-	return (
-		<View style={style.tagFriendsContainer}>
-			<ScrollView alwaysBounceHorizontal={false} horizontal={true} style={style.taggedFriendsScroll}>
-				{props.taggedFriends.map((taggedFriend) => (
-					<Image
-						key={taggedFriend.id}
-						source={{uri: taggedFriend.avatarURL}}
-						resizeMode={'cover'}
-						style={style.taggedFriendIcon}
-					/>
-				))}
-			</ScrollView>
-			<TouchableOpacity onPress={props.showTagFriendsModal} style={style.tagFriendsButton}>
-				<Image source={Icons.tagFriendSmall} />
-			</TouchableOpacity>
-		</View>
-	);
-};
+export const AddFriendsList: React.SFC<IAddFriendsListProps> = ({taggedFriends, showTagFriendsModal}) => (
+	<View style={style.tagFriendsContainer}>
+		<ScrollView alwaysBounceHorizontal={false} horizontal={true} style={style.taggedFriendsScroll}>
+			{taggedFriends.map((taggedFriend) => (
+				<Image
+					key={taggedFriend.id}
+					source={{uri: taggedFriend.avatarURL}}
+					resizeMode={'cover'}
+					style={style.taggedFriendIcon}
+				/>
+			))}
+		</ScrollView>
+		<TouchableOpacity onPress={showTagFriendsModal} style={style.tagFriendsButton}>
+			<Image source={Icons.tagFriendSmall} />
+		</TouchableOpacity>
+	</View>
+);
