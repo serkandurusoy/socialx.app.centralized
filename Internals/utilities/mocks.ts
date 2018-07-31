7a291dc7fe7f1e38c37e3fa53249ee6d9ac73e54
ionutmovila <ionut.movila@sensidev.com>
2018-07-31 13:15:59 +0300

    MD-260, added mock data for Comments screen

--- Internals/utilities/mocks.ts
+++ Internals/utilities/mocks.ts
@@ -124,3 +124,26 @@ export const SAMPLE_ACTIVITY_CARDS = [
 		requestId: '990325',
 	},
 ];
+
+export const SAMPLE_USER = {
+	userId: '585832b2-3ce5-4924-b75c-81e18bbf46de',
+	name: 'test user',
+	username: 'testname',
+	avatar: {
+		id: '2a098fd6-03b2-4b88-9bbb-8356727b480f',
+		hash: 'QmcPjeRF774dU5oMrF7j3ceuKJcwp8wKFwrdu74z6MnpsY',
+		type: 'image/jpeg',
+		size: 123,
+	},
+};
+
+export const MOCK_COMMENTS = [
+	{
+		id: 'comment_id_1',
+		owner: SAMPLE_USER,
+		createdAt: new Date().getTime() / 1000 - 2000,
+		text: 'Sample mock comment',
+		likes: [SAMPLE_USER],
+		comments: [],
+	},
+];
