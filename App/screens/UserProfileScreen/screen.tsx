14fb07e2780dd20bb9d943d4e937d043a2ef0d38
ionutmovila <ionut.movila@sensidev.com>
2018-08-30 17:45:07 +0300

    MD-380, minor updates after rebase

--- App/screens/UserProfileScreen/screen.tsx
+++ App/screens/UserProfileScreen/screen.tsx
@@ -5,7 +5,7 @@ import {DataProvider} from 'recyclerlistview';
 
 import {IWallPostCardProp, NoPhotos, PhotoGrid, ProfileTopContainer, WallPostCard} from 'components';
 import {PROFILE_TAB_ICON_TYPES} from 'consts';
-import {IWithLoaderProps, withInlineLoader} from 'hoc';
+import {IWithLoaderProps, WithInlineLoader} from 'hoc';
 import {Colors} from 'theme';
 import {IUserQuery, SearchResultKind} from 'types';
 
@@ -78,16 +78,16 @@ const UserProfileScreenComponent: React.SFC<IUserProfileScreenProps> = ({
 	activeTab,
 	containerHeight,
 	onLayoutChange,
+	isLoading,
 }) => {
 	const hasPhotos = numberOfPhotos !== 0;
 
 	let contentContainerStyle;
 	if (activeTab === PROFILE_TAB_ICON_TYPES.GRID && containerHeight !== 0) {
-		if (containerHeight < SCREEN_HEIGHT / 2) {
-			contentContainerStyle = [style.contentContainer, {height: SCREEN_HEIGHT / 2}];
-		} else {
-			contentContainerStyle = [style.contentContainer, {height: containerHeight}];
-		}
+		contentContainerStyle =
+			containerHeight < SCREEN_HEIGHT / 2
+				? [style.contentContainer, {height: SCREEN_HEIGHT / 2}]
+				: [style.contentContainer, {height: containerHeight}];
 	} else {
 		contentContainerStyle = style.contentContainer;
 	}
@@ -95,79 +95,81 @@ const UserProfileScreenComponent: React.SFC<IUserProfileScreenProps> = ({
 	const scrollContainerStyles = hasPhotos ? style.scrollContainer : [style.scrollContainer, {flex: 1}];
 
 	return (
-		<View style={style.container}>
-			<View style={[style.whiteBottomView, {height: SCREEN_HEIGHT / 2}]} />
-			<ScrollView
-				contentContainerStyle={scrollContainerStyles}
-				showsVerticalScrollIndicator={false}
-				refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} tintColor={Colors.white} />}
-				disabled={hasPhotos}
-			>
-				<ProfileTopContainer
-					avatarURL={avatarURL}
-					fullName={fullName}
-					username={username}
-					numberOfFollowers={numberOfFollowers}
-					numberOfFollowing={numberOfFollowing}
-					numberOfLikes={numberOfLikes}
-					numberOfPhotos={numberOfPhotos}
-					numberOfViews={numberOfViews}
-					onViewProfilePhoto={onViewProfilePhoto}
-					onAddFriend={onAddFriend}
-					onShowFriendshipOptions={onShowFriendshipOptions}
-					friendRequestStatus={friendRequestStatus}
-					ownUser={ownUser}
-					onIconPress={onIconPress}
-					aboutMeText={aboutMeText}
-					tabs={true}
-					activeTab={activeTab}
-				/>
-				{hasPhotos && (
-					<View style={contentContainerStyle}>
-						<Animated.View style={[style.postsContainer, {transform: [{translateX: listTranslate}]}]}>
-							{recentPosts.map((post, i) => {
-								const likedByMe = !!post.likes.find((like: IUserQuery) => like.userId === currentUserId);
+		<WithInlineLoader isLoading={isLoading}>
+			<View style={style.container}>
+				<View style={[style.whiteBottomView, {height: SCREEN_HEIGHT / 2}]} />
+				<ScrollView
+					contentContainerStyle={scrollContainerStyles}
+					showsVerticalScrollIndicator={false}
+					refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} tintColor={Colors.white} />}
+					disabled={hasPhotos}
+				>
+					<ProfileTopContainer
+						avatarURL={avatarURL}
+						fullName={fullName}
+						username={username}
+						numberOfFollowers={numberOfFollowers}
+						numberOfFollowing={numberOfFollowing}
+						numberOfLikes={numberOfLikes}
+						numberOfPhotos={numberOfPhotos}
+						numberOfViews={numberOfViews}
+						onViewProfilePhoto={onViewProfilePhoto}
+						onAddFriend={onAddFriend}
+						onShowFriendshipOptions={onShowFriendshipOptions}
+						friendRequestStatus={friendRequestStatus}
+						ownUser={ownUser}
+						onIconPress={onIconPress}
+						aboutMeText={aboutMeText}
+						tabs={true}
+						activeTab={activeTab}
+					/>
+					{hasPhotos && (
+						<View style={contentContainerStyle}>
+							<Animated.View style={[style.postsContainer, {transform: [{translateX: listTranslate}]}]}>
+								{recentPosts.map((post, i) => {
+									const likedByMe = !!post.likes.find((like: IUserQuery) => like.userId === currentUserId);
 
-								return (
-									<View style={style.wallPostContainer} key={i}>
-										<WallPostCard
-											{...post}
-											likedByMe={likedByMe}
-											canDelete={false}
-											onCommentClick={() => onCommentClick(post.id, null)}
-											onImageClick={(index: number) => onImageClick(index, post.media || post.Media)}
-											onLikeButtonClick={() => onLikeClick(likedByMe, post.id)}
-											noInput={true}
-										/>
-									</View>
-								);
-							})}
-						</Animated.View>
-						<Animated.View
-							onLayout={(event: any) => {
-								if (containerHeight !== event.nativeEvent.layout.height) {
-									onLayoutChange(event.nativeEvent.layout.height);
-								}
-							}}
-							style={[style.gridContainer, {transform: [{translateX: gridTranslate}]}]}
-						>
-							<PhotoGrid
-								loadMorePhotosHandler={loadMorePhotosHandler}
-								gridMediaProvider={gridMediaProvider}
-								onViewMediaFullScreen={onViewMediaFullScreen}
-								header={{
-									element: <View style={{width: 1, height: 1}} />,
-									height: hasPhotos ? 1 : SCREEN_HEIGHT,
+									return (
+										<View style={style.wallPostContainer} key={i}>
+											<WallPostCard
+												{...post}
+												likedByMe={likedByMe}
+												canDelete={false}
+												onCommentClick={() => onCommentClick(post.id, null)}
+												onImageClick={(index: number) => onImageClick(index, post.media || post.Media)}
+												onLikeButtonClick={() => onLikeClick(likedByMe, post.id)}
+												noInput={true}
+											/>
+										</View>
+									);
+								})}
+							</Animated.View>
+							<Animated.View
+								onLayout={(event: any) => {
+									if (containerHeight !== event.nativeEvent.layout.height) {
+										onLayoutChange(event.nativeEvent.layout.height);
+									}
 								}}
-								disabled={hasPhotos}
-							/>
-						</Animated.View>
-					</View>
-				)}
-				{!hasPhotos && <NoPhotos />}
-			</ScrollView>
-		</View>
+								style={[style.gridContainer, {transform: [{translateX: gridTranslate}]}]}
+							>
+								<PhotoGrid
+									loadMorePhotosHandler={loadMorePhotosHandler}
+									gridMediaProvider={gridMediaProvider}
+									onViewMediaFullScreen={onViewMediaFullScreen}
+									header={{
+										element: <View style={{width: 1, height: 1}} />,
+										height: hasPhotos ? 1 : SCREEN_HEIGHT,
+									}}
+									disabled={hasPhotos}
+								/>
+							</Animated.View>
+						</View>
+					)}
+					{!hasPhotos && <NoPhotos />}
+				</ScrollView>
+			</View>
+		</WithInlineLoader>
 	);
 };
 
-export default withInlineLoader(UserProfileScreenComponent);
+export default UserProfileScreenComponent;
